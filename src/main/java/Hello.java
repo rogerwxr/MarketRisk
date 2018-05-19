00b1b51e00ef687a53b7605484cf964b6e0a6d65
rogerwxr <mustwangrong@163.com>
2018-05-19 11:19:10 +0800

    unit test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,15 +1,18 @@
- public class Hello{
+public class Hello{
      public static void main(String[] args){
          for(int i=1;i<=100;i++)
     { 
-	if (i%3==0 && i%5==0)	{
-	System.out.println("FizzBuzz"); 
-	}else if (i%3==0)	{
-	System.out.println("Fizz"); 
-	}else if (i%5==0)	{
-	System.out.println("Buzz"); 
-	}else {
-	System.out.println(i); } 
+        	 System.out.println(show(i));
      }
  }
+     public static String show(int i) {  
+    		if (i%3==0 && i%5==0)	{
+    			return "FizzBuzz";
+    			}else if (i%3==0)	{
+    			return "Fizz";
+    			}else if (i%5==0)	{
+    			return "Buzz";
+    			}else {
+    			return Integer.toString(i); } 
+ 	} 
 }
---
+++ src/main/java/TestShow.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matcher.*; 
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Test;
+
+
+public class TestShow {
+
+	@Test
+	public void test3() {
+		String z = new Hello().show(3);         
+        assertThat(z, is("Fizz"));  
+	}
+	
+	@Test
+	public void test5() {
+		String z = new Hello().show(5);         
+        assertThat(z, is("Buzz"));  
+	}
+	@Test
+	public void test35() {
+		String z = new Hello().show(15);         
+        assertThat(z, is("Fizz"));  
+	}
+
+}
